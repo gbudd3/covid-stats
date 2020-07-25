b7f02f56faab06236e5a5a9a4dcdbd5c675ad90d
Budd, George <george.budd@firemon.com>
2020-07-25 15:49:41 -0400

    First pass at graphs

--- covid_morris.R
+++ covid_morris.R
@@ -24,3 +24,30 @@ morris <- morris %>% mutate(delta_cases = cases - lag(cases))
 morris <- morris %>% mutate(delta_deaths = deaths - lag(deaths))
 morris$mean7_delta_cases <- floor(rollmeanr(morris$delta_cases, 7, fill=NA))
 morris$mean7_delta_deaths <- floor(rollmeanr(morris$delta_deaths, 7, fill=NA))
+
+pdf("output/covid_nj.pdf", width = 11, height=8.5)
+
+# Adjust theme to center title
+theme_update(plot.title = element_text(hjust = 0.5))
+theme_update(plot.subtitle = element_text(hjust = 0.5))
+
+# Graph NJ Stats.  Cases / day (with rolling average) and Deaths / day
+print(ggplot(nj, aes(x=date_p))+
+      geom_bar(stat="identity", aes(y=delta_cases), color="blue", fill="white")+
+	  geom_line(stat="identity",aes(y=mean7_delta_cases), color="red", size=2)+
+      labs(title="NJ Cases / Day")+
+      labs(x="Date")+
+      labs(caption="Cases/Day")+
+      labs(y="Cases/Day")
+      )
+
+print(ggplot(nj, aes(x=date_p))+
+      geom_bar(stat="identity", aes(y=delta_deaths), color="blue", fill="white")+
+	  geom_line(stat="identity",aes(y=mean7_delta_deaths), color="red", size=2)+
+      labs(title="NJ Deaths / Day")+
+      labs(x="Date")+
+      labs(caption="Cases/Day")+
+      labs(y="Cases/Day")
+      )
+
+dev.off()
