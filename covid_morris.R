74485edadb3dfe86a78ba8cfb1a0ea73d2dad4f6
Budd, George <george.budd@firemon.com>
2020-07-25 20:05:31 -0400

    Clean up graphing to reduce duplication

--- covid_morris.R
+++ covid_morris.R
@@ -2,6 +2,28 @@ library(tidyverse)
 library(ggplot2)
 library(zoo)
 
+graph_covid <- function(data_frame, title) {
+# Graph Stats.  Cases / day (with rolling average) and Deaths / day
+
+	print(ggplot(data_frame, aes(x=date_p))+
+		geom_bar(stat="identity", aes(y=delta_cases), color="blue", fill="white")+
+		geom_line(stat="identity",aes(y=mean7_delta_cases), color="red", size=2)+
+		labs(title=paste(title, "Cases / Day"))+
+		labs(x="Date")+
+		labs(caption="Data from NY Times")+
+		labs(y="Cases/Day")
+		)
+
+	print(ggplot(data_frame, aes(x=date_p))+
+		geom_bar(stat="identity", aes(y=delta_deaths), color="blue", fill="white")+
+		geom_line(stat="identity",aes(y=mean7_delta_deaths), color="red", size=2)+
+		labs(title=paste(title, "Deaths / Day"))+
+		labs(x="Date")+
+		labs(caption="Data from NY Times")+
+		labs(y="Deaths/Day")
+		)
+}
+
 # Setup states and specifically NJ
 states <- read.csv("covid-19-data/us-states.csv")
 states$date_p <- as.POSIXct(states$date)
@@ -11,6 +33,7 @@ nj <- nj %>% mutate(delta_cases = cases - lag(cases))
 nj <- nj %>% mutate(delta_deaths = deaths - lag(deaths))
 nj$mean7_delta_cases <- floor(rollmeanr(nj$delta_cases, 7, fill=NA))
 nj$mean7_delta_deaths <- floor(rollmeanr(nj$delta_deaths, 7, fill=NA))
+nj$wday <- weekdays(nj$date_p)
 
 # Setup counties, specifically Morris county NJ
 counties <- read.csv("covid-19-data/us-counties.csv") 
@@ -24,6 +47,7 @@ morris <- morris %>% mutate(delta_cases = cases - lag(cases))
 morris <- morris %>% mutate(delta_deaths = deaths - lag(deaths))
 morris$mean7_delta_cases <- floor(rollmeanr(morris$delta_cases, 7, fill=NA))
 morris$mean7_delta_deaths <- floor(rollmeanr(morris$delta_deaths, 7, fill=NA))
+morris$wday <- weekdays(morris$date_p)
 
 pdf("output/covid_nj.pdf", width = 11, height=8.5)
 
@@ -31,42 +55,7 @@ pdf("output/covid_nj.pdf", width = 11, height=8.5)
 theme_update(plot.title = element_text(hjust = 0.5))
 theme_update(plot.subtitle = element_text(hjust = 0.5))
 
-# Graph NJ Stats.  Cases / day (with rolling average) and Deaths / day
-print(ggplot(nj, aes(x=date_p))+
-      geom_bar(stat="identity", aes(y=delta_cases), color="blue", fill="white")+
-	  geom_line(stat="identity",aes(y=mean7_delta_cases), color="red", size=2)+
-      labs(title="NJ Cases / Day")+
-      labs(x="Date")+
-      labs(caption="Data from NY Times")+
-      labs(y="Cases/Day")
-      )
+graph_covid(nj, "New Jersey")
+graph_covid(morris, "Morris County")
 
-print(ggplot(nj, aes(x=date_p))+
-      geom_bar(stat="identity", aes(y=delta_deaths), color="blue", fill="white")+
-	  geom_line(stat="identity",aes(y=mean7_delta_deaths), color="red", size=2)+
-      labs(title="NJ Deaths / Day")+
-      labs(x="Date")+
-      labs(caption="Data from NY Times")+
-      labs(y="Deaths/Day")
-      )
-
-# Graph Morris County Stats.  Cases / day (with rolling average) and Deaths / day
-print(ggplot(morris, aes(x=date_p))+
-      geom_bar(stat="identity", aes(y=delta_cases), color="blue", fill="white")+
-	  geom_line(stat="identity",aes(y=mean7_delta_cases), color="red", size=2)+
-      labs(title="Morris County Cases / Day")+
-      labs(x="Date")+
-      labs(caption="Data from NY Times")+
-      labs(y="Cases/Day")
-      )
-
-print(ggplot(morris, aes(x=date_p))+
-      geom_bar(stat="identity", aes(y=delta_deaths), color="blue", fill="white")+
-	  geom_line(stat="identity",aes(y=mean7_delta_deaths), color="red", size=2)+
-      labs(title="Morris County Deaths / Day")+
-      labs(x="Date")+
-      labs(caption="Data from NY Times")+
-      labs(y="Deaths/Day")
-      )
- 
 dev.off()
